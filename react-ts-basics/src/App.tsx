aa711bb1f3bbdd8e3d71b2f9a274f47adb2cbf6c
maziazi <mazis9651@gmail.com>
2024-03-11 23:43:33 +0700

    membuat komponenen dengan memanfaatkan bascis typescript

---
+++ react-ts-basics/src/App.tsx
@@ -0,0 +1,15 @@
+import CourseGoal from './components/CourseGoal.tsx';
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+export default function App(){
+  return (
+    <main>
+      <CourseGoal title="Belajar React dan TypeScript">
+        <p>Belajar ini dari Udemy kelas oleh Academia</p> 
+      </CourseGoal>
+    </main>
+  );
+}
---
+++ react-ts-basics/src/components/CourseGoal.tsx
@@ -0,0 +1,23 @@
+import { type PropsWithChildren} from "react";
+
+// interface CourseGoalProps{
+//     title: string,
+//     children: ReactNode,
+// }
+
+type CourseGoalProps = PropsWithChildren<{title: string}>;
+
+export default function CourseGoal({
+    title, 
+    children,
+}: CourseGoalProps) {
+    return (
+    <article>
+        <div>
+            <h2>{title}</h2>
+            {children}
+        </div>
+        <button>Delate</button>
+    </article>
+    );
+}
